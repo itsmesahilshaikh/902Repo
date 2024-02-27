940451ee32bc989d0cbc989dac9c1649022eb940
sahil <sahil@SahilShaikh>
2024-02-27 23:29:13 +0530

    task done for dev1

--- GuptProject/src/main/java/Proj1/GuptProject/App.java
+++ GuptProject/src/main/java/Proj1/GuptProject/App.java
@@ -10,5 +10,9 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("sahil shaikh");
+        ArithmeticOperations ar=new ArithmeticOperations();
+        System.out.println(ar.sum(100, 200));
+        System.out.println(ar.sub(100, 200));
+
     }
 }
--- GuptProject/src/main/java/Proj1/GuptProject/ArithmeticOperations.java
+++ GuptProject/src/main/java/Proj1/GuptProject/ArithmeticOperations.java
@@ -2,9 +2,11 @@ package Proj1.GuptProject;
 
 public class ArithmeticOperations {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public int sum(int a, int b ) {
+		return a+b;
 		
 	}
-
+	public  int sub(int x,int y ) {
+		return x-y;
+	}
 }
