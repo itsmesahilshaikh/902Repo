bb578ab86320051473c6d666d9bddb70d2784995
sahil <sahil@192.168.0.9>
2024-02-28 15:51:07 +0530

    task done for branch

--- GuptProject/src/main/java/Proj1/register/ArithmeticOperations.java
+++ GuptProject/src/main/java/Proj1/register/ArithmeticOperations.java
@@ -2,4 +2,10 @@ package Proj1.register;
 
 public class ArithmeticOperations {
 
+	public String doCrediCardPayment() {
+		return "Credit Card Payment is Done";
+	}
+	public String doDebitCardPayment() {
+		return "Debit Card Payment is Done";
+	}
 }
